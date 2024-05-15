b9467c65717b83d3765d9589c973bc39bef69832
gottsu3 <134078312+gottsu3@users.noreply.github.com>
2024-05-15 20:20:42 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('verify_captcha/', views.verify_captcha, name='verify_captcha'),
-    # Dodaj inne trasy URL tutaj, jeśli potrzebujesz
-]
